55eb31d1246c8659ae51ee2ac12d5849072019fb
sebastians9517 <sebastians9517@gmail.com>
2020-12-04 00:17:17 -0600

    add flight Schema

--- models/flight.js
+++ models/flight.js
@@ -1,1 +1,18 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema({
+    airline: {type: String,
+              required: true,
+              enum: ['American', 'Southwest', 'United'],
+    airport: {type: String,
+              required: true,
+              enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN'},
+    flightNo: {type: Number,
+               required: true, min: 10, max: 9999},
+    departs: {type: Date,
+              required: true,
+              default: () => {
+                  return moment().add(1, 'year')
+              }}}
+})
