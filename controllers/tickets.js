d78fdfa1fd7826ec4f7d811586c0e41a7eb194b1
sebastians9517 <sebastians9517@gmail.com>
2020-12-07 21:52:58 -0600

    finished bonus delete ticket

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,26 +1,25 @@
-// const Flight = require('../models/flight');
-
-// module.exports = {
-//     create
-// }
-
-// function create(req, res) {
-//     Flight.findById(req.params.id, (err, flight) => {
-//         flight.tickets.push(req.body)
-//         flight.save((err) => {
-//             res.redirect(`/flights/${flights._id}`);
-//         });
-//     });
-// };
 
 const Flight = require('../models/flight')
 module.exports = {
-    create
+    create,
+    deleteTicket
 }
 function create(req, res) {
     Flight.findById(req.params.id, function(err, flight){
         flight.tickets.push(req.body)
         flight.save(function(err) {
+            res.redirect(`/flights/${flight._id}`)
+        })
+    })
+}
+
+function deleteTicket(req, res){
+    Flight.findById(req.params.flightId)
+    .then((flight) => {
+        const idx = flight.tickets.findIndex(ticket => ticket._id == req.params.ticketId)
+        flight.tickets.splice(idx, 1)
+        flight.save()
+        .then(()=> {
             res.redirect(`/flights/${flight._id}`)
         })
     })
